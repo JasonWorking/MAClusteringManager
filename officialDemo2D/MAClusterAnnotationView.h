56262ceeca82f2407cbd5ae9f97f6bca867f31c8
Jason <331314708@qq.com>
2015-03-13 23:44:14 +0800

    Add descriptions

--- officialDemo2D/MAClusterAnnotationView.h
+++ officialDemo2D/MAClusterAnnotationView.h
@@ -8,12 +8,25 @@
 
 #import <MAMapKit/MAMapKit.h>
 
+
+/**
+ *  
+    -----
+   /     \
+  |   2   |
+   \     /
+    -----
+ */
+
 @interface MAClusterAnnotationView : MAAnnotationView
 
+// The count of annotations in this cluster, will display as a number on the annotation view.
 @property (nonatomic, assign) NSUInteger count;
 
+// The fill color of the annotationView
 @property (nonatomic, strong) UIColor *clusterColor;
 
+// The number label's text color of the annotationView
 @property (nonatomic, strong) UIColor *textColor;
 
 @end
